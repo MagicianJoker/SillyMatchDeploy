cc0b160f532186bb79037550e3ed8a4577e07603
MagicianJoker <1159446117@qq.com>
2024-10-26 14:16:48 +0800

    6

--- bot.ts
+++ bot.ts
@@ -7,6 +7,12 @@ bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 bot.command("ping", (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`));
 
 bot.command("play", (ctx) => {ctx.replyWithGame("matchgame")});
+bot.on("callback_query:game_short_name", async (ctx) => {
+    console.log("ctx.callbackQuery.game_short_name = ",ctx.callbackQuery.game_short_name)
+    if (ctx.callbackQuery.game_short_name === "matchgame") {
+        await ctx.answerCallbackQuery({ url: "https://magicianjoker.icu/CocosSillyMatch/web-mobile/" });
+    }
+});
 
 // 启动你的 bot
 bot.start();
