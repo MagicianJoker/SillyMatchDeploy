3315e17135f0c9af168dfa10ed2a38a6dc2b746c
MagicianJoker <1159446117@qq.com>
2024-10-26 13:31:09 +0800

    1

--- bot.ts
+++ bot.ts
@@ -5,3 +5,6 @@ export const bot = new Bot("8096312892:AAGHPLucQh-X11xf0Vm532TH2B0uJvbyGHA");
 bot.command("start", (ctx) => ctx.reply("Welcome! Up and running."));
 
 bot.command("ping", (ctx) => ctx.reply(`Pong! ${new Date()} ${Date.now()}`));
+
+// 启动你的 bot
+bot.start();
